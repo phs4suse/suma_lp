ee9329772563e99e8560bc794106c05c8ef1a532
Patrick Swartz <patrick.swartz@suse.com>
2020-08-31 14:50:43 -0500

    Create mk_lifecycle_2.sh
    This creates a simple 2 column of the Livepatch kernel and the expiration date csv file to be used with the mk_html.sh script.

---
+++ mk_lifecycle_2.sh
@@ -0,0 +1,8 @@
+!/bin/bash
+
+# Clean up data file for proper conversion and create our HTML output 
+cat var/lib/lifecycle/data/sle-live-patching.lifecycle usr/share/lifecycle/data/sle-module-live-patching.lifecycle >> 12-15combined_live-patching.txt
+grep -v '^#' 12-15combined_live-patching.txt >> $1
+
+sed -i '1s/^/LivePatch\ Kernel\,Expire\ Date \n/' $1
+sed -i s/\*,//g $1
